7dc59201d8e617809dc4a908db59e49a76a639bc
Pawel Tarapata <01197256@pw.edu.pl>
2026-06-28 15:04:12 +0200

    JUPI

---
+++ calculator/src/main/java/com/ficsit/calculator/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.ficsit.calculator;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        // Wywołujemy prawdziwego Maina z JavaFX
+        Main.main(args);
+    }
+}
